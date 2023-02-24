765714c3e938765e88b5898d16764f8a9956cde1
mr nim <mrnim@nimtechnology.com>
2023-02-25 01:03:45 +0700

    refactor code

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"cyclops/log"
-	"github.com/fsnotify/fsnotify"
 	"os"
 	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func init() {
@@ -13,6 +14,8 @@ func init() {
 	os.Setenv("TZ", "Asia/Ho_Chi_Minh")
 
 }
+
+var watcher *fsnotify.Watcher
 
 // main
 func main() {
@@ -25,37 +28,12 @@ func main() {
 		log.Info("Cyclops look at path ", cyclops)
 	}
 	// creates a new file watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
+	watcher, _ = fsnotify.NewWatcher()
 	defer watcher.Close()
 
-	// Watch the specified directory and all subdirectories.
-	err = filepath.Walk(cyclops, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			// Ignore "no such file or directory" errors
-			if os.IsNotExist(err) {
-				log.Error("Ignored ", err)
-				return nil
-			}
-			log.Error("Error accessing file or directory:", err)
-			return err
-		}
-
-		if info.IsDir() {
-			err := watcher.Add(path)
-			if err != nil {
-				log.Error(err)
-			}
-			log.Info("Found Directory:", path)
-		} else {
-			log.Info("Found file:", path)
-		}
-
-		return nil
-	})
-	if err != nil {
+	// starting at the root of the project, walk each file/directory searching for
+	// directories
+	if err := filepath.Walk(cyclops, watchDir); err != nil {
 		log.Error("Error walking directory:", err)
 	}
 
@@ -82,4 +60,29 @@ func main() {
 
 	// Block main goroutine forever.
 	<-make(chan struct{})
+}
+
+// watchDir gets run as a walk func, searching for directories to add watchers to
+func watchDir(path string, fi os.FileInfo, err error) error {
+	if err != nil {
+		// Ignore "no such file or directory" errors
+		if os.IsNotExist(err) {
+			log.Error("Ignored ", err)
+			return nil
+		}
+		log.Error("Error accessing file or directory:", err)
+		return err
+	}
+
+	if fi.IsDir() {
+		err := watcher.Add(path)
+		if err != nil {
+			log.Error(err)
+		}
+		log.Info("Found Directory:", path)
+	} //else {
+	//	log.Info("Found file:", path)
+	//}
+
+	return nil
 }
